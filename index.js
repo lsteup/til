8ccf03fb602dbf746b15ea653c514450395e4a32
lsteup <loras12@gmail.com>
2024-06-10 17:20:28 +0200

    create project skeleton

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("til server");
+});
+
+//routers
+
+// error handler
+
+app.use(express.json());
+
+// routes
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    //await connectDB(process.env.MONGO_CODE);
+    app.listen(port, () => {
+      console.log("server is lening on port ", port);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
